37e4556cd3c8a01b080d7d8caee605245c55e0f6
Shivanshu <shivanshukumargupta6969@gmail.com>
2025-03-29 12:34:07 -0700

    Initial commit with gitattribute file

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ZEESHU</div>
+          <div className="right">
+            <p>Z6 Gulshan-e-Dawat, Delhi</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Shivanshu</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By CodeWithShivanshu.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
